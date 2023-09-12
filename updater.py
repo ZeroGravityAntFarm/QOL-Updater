4df07023d2089807e603de0681cf1e609eae63a9
josh <itzdi0de@gmail.com>
2023-09-12 17:18:03 -0400

    big ole honkin commit

--- updater.py
+++ updater.py
@@ -1,23 +1,23 @@
-import tkinter
-import tkinter.messagebox
 import customtkinter
 from PIL import ImageTk
 import os
 import GPUtil
 import json
 import subprocess
-import signal
-import threading
 import time
 import pyi_splash
+
+from tkinter import filedialog
+from pathlib import Path
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
-vulkan_data = []
+vulkan_data = ""
 
 
+#Get gpu hardware details and validate vulkan support
 def check_gpu_support():
     systemInfo = {}
 
@@ -41,11 +41,7 @@ def check_gpu_support():
     return systemInfo
 
 
-def get_vulkan_thread():
-    vThread = threading.Thread(target=get_vulkan_version)
-    vThread.start()
-
-
+#Get vulkan version on local machine
 def get_vulkan_version():
     global vulkan_data
     
@@ -56,19 +52,13 @@ def get_vulkan_version():
     vulkanInfo = subprocess.Popen(["assets\\vulkaninfo.exe"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, creationflags=subprocess.CREATE_NO_WINDOW)
     stdout, stderr = vulkanInfo.communicate()
 
-    '''
-    if stderr:
-        
-        vulkan_data.append(stderr)
-        vulkanInfo.kill()
-        vulkanInfo.wait()
 
-        return
-    '''
-    
     for line in stdout.splitlines():
         if "Vulkan Instance Version:" in str(line):
-            vulkan_data.append(line)
+            line = line.decode('UTF-8')
+            line = line.split(": ")[1]
+
+            vulkan_data = line
             break
     
     vulkanInfo.kill()
@@ -81,139 +71,110 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # configure window
+        #Configure Window
         self.title("QOL Updater")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{800}x{650}")
 
-        # configure grid layout (4x4)
+        #Configure grid
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3, 4), weight=0)
         self.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
-        # create sidebar frame with widgets
+        # Sidebar Widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
+
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="QOL 6", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.open_ed_directory)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
 
-        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_4.grid(row=7, column=0, padx=20, pady=10)
 
-
-        '''
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-        '''
-        # create textbox
-        self.textbox = customtkinter.CTkTextbox(self, width=250)
-        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self, width=250)
-        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("DXVK")
-        self.tabview.add("D3D9")
-        self.tabview.add("dewrito.json")
-        self.tabview.tab("DXVK").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("D3D9").grid_columnconfigure(0, weight=1)
-
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("DXVK"), dynamic_resizing=False,
-                                                        values=["Value 1", "Value 2", "Value Long Long Long"])
-        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("DXVK"),
-                                                    values=["Value 1", "Value 2", "Value Long....."])
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("DXVK"), text="Open CTkInputDialog",
-                                                           command=self.open_input_dialog_event)
-        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("D3D9"), text="CTkLabel on Tab 2")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
-
-        '''
-        # create slider and progressbar frame
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-        self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
-        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-        self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-        self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-        self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
-        '''
-
-        '''
-        # create scrollable frame
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
-        self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
-        for i in range(100):
-            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-            self.scrollable_frame_switches.append(switch)
-        '''
-
-        '''
-        # create checkbox and switch frame
-        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
-        '''
-
-        # set default values
+        #Configure Sidbar Buttons
         self.sidebar_button_1.configure(text="Apply Updates")
         self.sidebar_button_2.configure(text="Verify Files")
         self.sidebar_button_3.configure(text="Revert Changes")
-        #self.checkbox_3.configure(state="disabled")
-        #self.checkbox_1.select()
-        #self.scrollable_frame_switches[0].select()
-        #self.scrollable_frame_switches[4].select()
-        self.appearance_mode_optionemenu.set("Dark")
-        self.optionmenu_1.set("CTkOptionmenu")
-        self.combobox_1.set("CTkComboBox")
-        #self.slider_1.configure(command=self.progressbar_2.set)
-        #self.slider_2.configure(command=self.progressbar_3.set)
-        #self.progressbar_1.configure(mode="indeterminnate")
-        #self.progressbar_1.start()
-        gpu_support = check_gpu_support()
+        self.sidebar_button_4.configure(text="Discord")
 
-        for key, value in gpu_support.items():
-            self.textbox.insert("0.0", key + ": " + str(value) + "\n")
+        #Textbox for logging
+        self.textbox = customtkinter.CTkTextbox(self, width=250)
+        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), rowspan=4, sticky="nsew")
 
-        get_vulkan_version()
-        time.sleep(2)
+        #Tabs for advanced config
+        self.tabview = customtkinter.CTkTabview(self, width=250)
+        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.textbox.insert("0.0", "Vulkan version: " + str(vulkan_data) + "\n")
-        #self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        #self.seg_button_1.set("Value 2")
+        self.tabview.add("DXVK")
+        self.tabview.add("CEF")
+        self.tabview.add("Stats")
+        self.tabview.add("FMM")
+        self.tabview.add("Scheme")
 
+
+        self.tabview.tab("DXVK").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("CEF").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Stats").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("FMM").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Scheme").grid_columnconfigure(0, weight=1)
+
+
+        # DXVK Checkbox
+        self.checkbox_DXVK_1 = customtkinter.CTkCheckBox(master=self.tabview.tab("DXVK"))
+        self.checkbox_DXVK_1.grid(row=0, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_DXVK_1.configure(text="DXVK")
+        self.checkbox_DXVK_1.select()
+
+        self.checkbox_DXVK_2 = customtkinter.CTkCheckBox(master=self.tabview.tab("DXVK"))
+        self.checkbox_DXVK_2.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_DXVK_2.configure(text="DXVK Cache")
+        self.checkbox_DXVK_2.select()
+
+        # CEF Checkbox
+        self.checkbox_CEF_1 = customtkinter.CTkCheckBox(master=self.tabview.tab("CEF"))
+        self.checkbox_CEF_1.grid(row=0, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_CEF_1.configure(text="CEF Fixes")
+        self.checkbox_CEF_1.select()
+
+        # Stats Checkbox
+        self.checkbox_STATS_1 = customtkinter.CTkCheckBox(master=self.tabview.tab("Stats"))
+        self.checkbox_STATS_1.grid(row=0, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_STATS_1.configure(text="zgaf.io Stat Reporting")
+        self.checkbox_STATS_1.select()
+
+        self.checkbox_STATS_3 = customtkinter.CTkCheckBox(master=self.tabview.tab("Stats"))
+        self.checkbox_STATS_3.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_STATS_3.configure(text="Add Active Masters")
+        self.checkbox_STATS_3.select()
+
+        self.checkbox_STATS_2 = customtkinter.CTkCheckBox(master=self.tabview.tab("Stats"))
+        self.checkbox_STATS_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_STATS_2.configure(text="Remove Dead Masters")
+        self.checkbox_STATS_2.select()
+
+        # FMM Checkbox
+        self.checkbox_FMM_1 = customtkinter.CTkCheckBox(master=self.tabview.tab("FMM"))
+        self.checkbox_FMM_1.grid(row=0, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_FMM_1.configure(text="Update FMM")
+        self.checkbox_FMM_1.select()
+
+        # Scheme Checkbox
+        self.checkbox_Scheme_1 = customtkinter.CTkCheckBox(master=self.tabview.tab("Scheme"))
+        self.checkbox_Scheme_1.grid(row=0, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_Scheme_1.configure(text="Add Pauwlo's Eldewrito Scheme Handler")
+        self.checkbox_Scheme_1.select()
+
+
+    #Functions for button events
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
@@ -224,6 +185,40 @@ class App(customtkinter.CTk):
     def sidebar_button_event(self):
         print("sidebar_button click")
 
+    #Grab and validate game directory
+    def open_ed_directory(self):
+        file_path = filedialog.askdirectory()
+        ed_path = Path(file_path + "/eldorado.exe")
+
+        if ed_path.is_file():
+            self.log("Eldorado found!", self.textbox)
+
+            #Get GPU and Vulkan support information
+            gpu_support = check_gpu_support()
+
+            for key, value in gpu_support.items():
+                self.log(key + ": " + str(value), self.textbox)
+
+            get_vulkan_version()
+
+            #Give vulkaninfo enough time to run
+            time.sleep(2)
+            self.log("Vulkan version: " + str(vulkan_data), self.textbox)
+
+        else:
+            self.textbox.insert(customtkinter.END, "Doesn't seem to be a valid ED directory...")
+            self.log("Doesn't seem to be a valid ED directory...", self.textbox)
+
+        return file_path
+
+    #Log to our textbox
+    def log(self, message, textbox):
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        textbox.insert(customtkinter.END, f"[{current_time}] - {message}\n")
+        self.textbox.see(customtkinter.END)
+
+        
 
 if __name__ == "__main__":
     app = App()
